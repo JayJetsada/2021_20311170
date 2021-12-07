0da1bce052a2e5333544d6260f695f5a53b9e5c4
JayJetsada <eeyjj7@nottingham.ac.uk>
2021-12-07 13:42:20 +0000

    WS5 2

---
+++ Worksheet5/exercise2/hello_qt_cm/build/CMakeFiles/hello_qt.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Jetsa/OneDrive/Documents/EEEE2046/2021_20311170/Worksheet5/exercise2/hello_qt_cm/build/hello_qt_autogen/mocs_compilation.cpp" "CMakeFiles/hello_qt.dir/hello_qt_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/hello_qt.dir/hello_qt_autogen/mocs_compilation.cpp.obj.d"
+  "C:/Users/Jetsa/OneDrive/Documents/EEEE2046/2021_20311170/Worksheet5/exercise2/hello_qt_cm/main.cpp" "CMakeFiles/hello_qt.dir/main.cpp.obj" "gcc" "CMakeFiles/hello_qt.dir/main.cpp.obj.d"
+  "C:/Users/Jetsa/OneDrive/Documents/EEEE2046/2021_20311170/Worksheet5/exercise2/hello_qt_cm/mainwindow.cpp" "CMakeFiles/hello_qt.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/hello_qt.dir/mainwindow.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Worksheet5/exercise2/hello_qt_cm/build/CMakeFiles/hello_qt.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_qt_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/hello_qt_autogen.dir/ParseCache.txt"
+  "hello_qt_autogen"
+  "CMakeFiles/hello_qt.dir/hello_qt_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/hello_qt.dir/hello_qt_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/hello_qt.dir/main.cpp.obj"
+  "CMakeFiles/hello_qt.dir/main.cpp.obj.d"
+  "CMakeFiles/hello_qt.dir/mainwindow.cpp.obj"
+  "CMakeFiles/hello_qt.dir/mainwindow.cpp.obj.d"
+  "hello_qt.exe"
+  "hello_qt.exe.manifest"
+  "hello_qt.pdb"
+  "libhello_qt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_qt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

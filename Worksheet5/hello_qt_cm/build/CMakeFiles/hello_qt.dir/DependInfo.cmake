690f33699e94a63134e17ca6a7c237d79fa437d3
JayJetsada <eeyjj7@nottingham.ac.uk>
2021-12-07 12:00:17 +0000

    WS5

---
+++ Worksheet5/hello_qt_cm/build/CMakeFiles/hello_qt.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Jetsa/OneDrive/Documents/EEEE2046/2021_20311170/Worksheet5/hello_qt_cm/hello_qt.cpp" "CMakeFiles/hello_qt.dir/hello_qt.cpp.obj" "gcc" "CMakeFiles/hello_qt.dir/hello_qt.cpp.obj.d"
+  "C:/Users/Jetsa/OneDrive/Documents/EEEE2046/2021_20311170/Worksheet5/hello_qt_cm/build/hello_qt_autogen/mocs_compilation.cpp" "CMakeFiles/hello_qt.dir/hello_qt_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/hello_qt.dir/hello_qt_autogen/mocs_compilation.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Worksheet5/hello_qt_cm/build/CMakeFiles/hello_qt.dir/hello_qt_autogen/mocs_compilation.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/hello_qt.dir/hello_qt_autogen/mocs_compilation.cpp.obj: \
+ C:\Users\Jetsa\OneDrive\Documents\EEEE2046\2021_20311170\Worksheet5\hello_qt_cm\build\hello_qt_autogen\mocs_compilation.cpp

36b1b86b1b28241a05798ed826a98e239e71152e
JayJetsada <eeyjj7@nottingham.ac.uk>
2022-04-04 19:54:50 +0100

    fix

--- Worksheet7/maths.cpp
+++ Worksheet7/maths.cpp
@@ -1,6 +1,5 @@
-// calc_cmake/adder.cpp ---------------------------------
+
 #include "maths.h"
 int add( int a, int b ) {
-	return a + b;
+return a + b;
 }
-// /calc_cmake/adder.cpp --------------------------------

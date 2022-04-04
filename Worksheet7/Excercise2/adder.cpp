29f609b29086286f0e669146c24cacd044a26e43
JayJetsada <eeyjj7@nottingham.ac.uk>
2022-04-04 16:05:19 +0100

    ws7

---
+++ Worksheet7/Excercise2/adder.cpp
@@ -0,0 +1,6 @@
+// adder.cpp ---------------------------------
+#include "adder.h"
+int add( int a, int b ) {
+return a + b;
+}
+// /adder.cpp --------------------------------
---
+++ Worksheet7/Excercise2/test1.cpp
@@ -0,0 +1,11 @@
+// test1.cpp
+#include <iostream>
+#include "adder.h"
+    
+int main(int argc, char *argv[])
+{
+   int a = 2;
+   int b = 5;
+   int c = add(a,b); 
+   return (c == 7) ? 0 : 1;
+}

d89506c7901c68e1470d4cb3dddb5bbf6587c490
JayJetsada <eeyjj7@nottingham.ac.uk>
2022-04-04 19:44:30 +0100

    fix

---
+++ Worksheet7/maths.cpp
@@ -0,0 +1,6 @@
+// calc_cmake/adder.cpp ---------------------------------
+#include "maths.h"
+int add( int a, int b ) {
+	return a + b;
+}
+// /calc_cmake/adder.cpp --------------------------------
---
+++ Worksheet7/maths.h
@@ -0,0 +1,15 @@
+// maths.h
+#ifndef MATHSLIB_MATHS_H
+#define MATHSLIB_MATHS_H
+/** @file
+<<<<<<< HEAD
+* This file contains the declarations of all exported functions. (Test2)
+=======
+* This file contains the declarations of all exported functions. (Test1)
+>>>>>>> 18237eab1c94d893dff79a2116acd4dad60c90b0
+*/
+/** Brief description
+* This description will hopefully be picked up on by doxygen
+*/
+int add(int a, int b);
+#endif

434bcf406a5b1af2c4d46bfbeb26463f7c851bb0
JayJetsada <eeyjj7@nottingham.ac.uk>
2022-04-05 09:46:58 +0100

    comment

--- Worksheet7/maths.h
+++ Worksheet7/maths.h
@@ -2,5 +2,11 @@
 #ifndef MATHSLIB_MATHS_H
 #define MATHSLIB_MATHS_H
 
+/** This function adds two numbers
+ *
+ * @param a is the first number to add
+ * @param b is the second number to add
+ * @return answer
+ */
 int add(int a, int b);
 #endif
